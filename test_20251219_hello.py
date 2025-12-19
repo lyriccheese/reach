58200c5aa4467720be22555cef227a2fba3cba39
lyriccheese <2240821082@qq.com>
2025-12-19 16:15:22 +0800

    Initial commit

---
+++ test_20251219_hello.py
@@ -0,0 +1,11 @@
+# message = "Hello Python world!"
+# print(message)
+    
+# message = "Hello Python Crash Course world!"
+# print(message)
+
+name = "ada lovelace"
+print(name.title())
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[-1])
